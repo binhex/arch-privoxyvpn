53b893b6518ae8d9cd8af617ebb6fd95c456fc5f
binhex <megalith01@gmail.com>
2019-07-19 13:10:33 +0100

    put system scripts on path

--- build/root/install.sh
+++ build/root/install.sh
@@ -21,7 +21,7 @@ fi
 aur_packages=""
 
 # call aur install script (arch user repo)
-source /root/aur.sh
+source aur.sh
 
 # container perms
 ####
@@ -75,7 +75,7 @@ EOF
 sed -i '/# PERMISSIONS_PLACEHOLDER/{
     s/# PERMISSIONS_PLACEHOLDER//g
     r /tmp/permissions_heredoc
-}' /root/init.sh
+}' init.sh
 rm /tmp/permissions_heredoc
 
 # env vars
@@ -263,7 +263,7 @@ EOF
 sed -i '/# ENVVARS_PLACEHOLDER/{
     s/# ENVVARS_PLACEHOLDER//g
     r /tmp/envvars_heredoc
-}' /root/init.sh
+}' init.sh
 rm /tmp/envvars_heredoc
 
 # cleanup
