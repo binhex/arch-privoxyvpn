00f8503c06cfd415ac95256274c06bc35cc87633
binhex <megalith01@gmail.com>
2019-09-19 15:34:40 +0100

    need to background microsocks

--- run/nobody/microsocks.sh
+++ run/nobody/microsocks.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "[info] Attempting to start microsocks..."
-/usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -u "${SOCKS_USER}" -P "${SOCKS_PASS}" -b "${vpn_ip}"
+nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -u "${SOCKS_USER}" -P "${SOCKS_PASS}" -b "${vpn_ip}" &
+echo "[info] microsocks process started"
+
