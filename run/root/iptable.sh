8b1c480cf922d3a743fe98ae486d558a6e405b29
sebstrgg <sebastian@wollter.nu>
2020-05-02 23:52:10 +0200

    Update iptable.sh
    added variables to iptables nat rule for redirecting an open port at vpn provider to plex port.

--- run/root/iptable.sh
+++ run/root/iptable.sh
@@ -118,7 +118,7 @@ iptables -P FORWARD DROP
 ip6tables -P FORWARD DROP 1>&- 2>&-
 
 # set policy to nat traffic from opened port @ vpn provider to plex
-iptables -t nat -I PREROUTING -p tcp --dport 60474 -j REDIRECT --to-ports 32400
+iptables -t nat -I PREROUTING -p tcp --dport $PLEX_NAT_PORT_VPN -j REDIRECT --to-ports $PLEX_NAT_PORT -m comment --comment "nat and redirect plex port"
 
 # output iptable rules
 ###
