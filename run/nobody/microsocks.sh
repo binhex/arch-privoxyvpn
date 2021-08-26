0938c4706664f54cac6ef31fa2c5859010fd2c0f
Muppetteer <Muppetteer@users.noreply.github.com>
2021-08-26 13:48:11 +1000

    Update microsocks.sh
    Run microsocks without authentication

--- run/nobody/microsocks.sh
+++ run/nobody/microsocks.sh
@@ -2,16 +2,8 @@
 
 echo "[info] Attempting to start microsocks..."
 if [[ "${VPN_ENABLED}" == "yes" ]]; then
-	if [[ -n "${SOCKS_USER}" ]]; then
-		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -u "${SOCKS_USER}" -P "${SOCKS_PASS}" -b "${vpn_ip}" &
-	else
-		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -b "${vpn_ip}" &
-	fi
+	nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -b "${vpn_ip}" &
 else
-	if [[ -n "${SOCKS_USER}" ]]; then
-		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -u "${SOCKS_USER}" -P "${SOCKS_PASS}" &
-	else
-		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 &
-	fi
+	nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 &
 fi
 echo "[info] microsocks process started"
