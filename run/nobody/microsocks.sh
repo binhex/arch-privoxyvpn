4ffc2336159b5dad9d4616a3b72a779c04acab41
Muppetteer <Muppetteer@users.noreply.github.com>
2021-08-26 13:53:16 +1000

    Update microsocks.sh
    Revert previous commit

--- run/nobody/microsocks.sh
+++ run/nobody/microsocks.sh
@@ -2,8 +2,16 @@
 
 echo "[info] Attempting to start microsocks..."
 if [[ "${VPN_ENABLED}" == "yes" ]]; then
-	nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -b "${vpn_ip}" &
+	if [[ -n "${SOCKS_USER}" ]]; then
+		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -u "${SOCKS_USER}" -P "${SOCKS_PASS}" -b "${vpn_ip}" &
+	else
+		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -b "${vpn_ip}" &
+	fi
 else
-	nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 &
+	if [[ -n "${SOCKS_USER}" ]]; then
+		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 -u "${SOCKS_USER}" -P "${SOCKS_PASS}" &
+	else
+		nohup /usr/local/bin/microsocks -i "0.0.0.0" -p 9118 &
+	fi
 fi
 echo "[info] microsocks process started"
