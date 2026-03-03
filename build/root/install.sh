b0385695e70ddfa549ae7722b1ea2d5845c4a63f
binhex <megalith01@gmail.com>
2026-03-03 23:52:25 +0000

    remove unneeded chown

--- build/root/install.sh
+++ build/root/install.sh
@@ -33,24 +33,6 @@ fi
 # write APPNAME and RELEASETAG to file to record the app name and release tag used to build the image
 echo -e "export APPNAME=${APPNAME}\nexport IMAGE_RELEASE_TAG=${RELEASETAG}\n" >> '/etc/image-build-info'
 
-# note do NOT download build scripts - inherited from int script with envvars common defined
-
-
-# pacman packages
-####
-
-# define pacman packages
-pacman_packages=""
-
-# install compiled packages using pacman
-if [[ -n "${pacman_packages}" ]]; then
-	# arm64 currently targetting aor not archive, so we need to update the system first
-	if [[ "${TARGETARCH}" == "arm64" ]]; then
-		pacman -Syu --noconfirm
-	fi
-	pacman -S --needed $pacman_packages --noconfirm
-fi
-
 # container perms
 ####
 
@@ -75,18 +57,6 @@ install_paths=$(echo "${install_paths}" | tr ',' ' ')
 
 # set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
 chmod -R 775 ${install_paths}
-
-# In install.sh heredoc, replace the chown section:
-cat <<EOF > /tmp/permissions_heredoc
-install_paths="${install_paths}"
-EOF
-
-# replace permissions placeholder string with contents of file (here doc)
-sed -i '/# PERMISSIONS_PLACEHOLDER/{
-    s/# PERMISSIONS_PLACEHOLDER//g
-    r /tmp/permissions_heredoc
-}' /usr/bin/init.sh
-rm /tmp/permissions_heredoc
 
 # env vars
 ####
